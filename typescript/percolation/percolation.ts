67b0366787888d175671539e1f80a68f4f75d3d7
Aleksi Moisio <aleksi.moisio30@gmail.com>
2020-03-20 14:51:30 +0200

    Add top and bottom index placeholders. Add sites array.

--- typescript/percolation/percolation.ts
+++ typescript/percolation/percolation.ts
@@ -2,8 +2,26 @@ import iUnionFind, { UnionFindConstructor } from "unionFind/iUnionFind";
 
 export default class Percolation {
 
+    /**
+     * Union find algorithm used to solve the percolation problem
+     */
     private readonly _unionFind: iUnionFind;
+    /**
+     * One dimensions of the percolation grid 
+     */
     private readonly _n: number;
+    /**
+     * 
+     */
+    private readonly _sites: Array<boolean>;
+    /**
+     * Represents the top-most virtual item
+     */
+    private readonly _top: number;
+    /**
+     * Represents the bottom-most virtual item
+     */
+    private readonly _bottom: number;
 
     // creates n-by-n grid, with all sites initially blocked
     constructor(n: number, unionFind: UnionFindConstructor) {
@@ -11,17 +29,37 @@ export default class Percolation {
             throw new Error("Invalid argument");
 
         this._n = n;
-        this._unionFind = new unionFind(n * n + 2);
 
-        // The last two array places are reserved for virtual
-        // items used for testing whether system percolates
-        // let count = n * n + 2;
-        // this._data = new Array<number>(count);
-        // for(let i = 0; i < count; i++) {
-        //     this._data[i] = i;
-        // }
+        this._unionFind = new unionFind(n * n + 2);
+        this._top = n * n + 1;
+        this._bottom = n * n + 2;
+        this._sites = new Array<boolean>(n * n);
     }
+
+    /**
+     * We model a percolation system using an n-by-n grid 
+     * of sites. Each site is either open or blocked. 
+     * A full site is an open site that can be connected 
+     * to an open site in the top row via a chain of 
+     * neighboring (left, right, up, down) open sites. 
+     * We say the system percolates if there is a full 
+     * site in the bottom row. In other words, a system 
+     * percolates if we fill all open sites connected to 
+     * the top row and that process fills some open site 
+     * on the bottom row. (For the insulating/metallic 
+     * materials example, the open sites correspond to 
+     * metallic materials, so that a system that 
+     * percolates has a metallic path from top to bottom, 
+     * with full sites conducting. For the porous substance 
+     * example, the open sites correspond to empty space 
+     * through which water might flow, so that a system 
+     * that percolates lets water fill open sites, flowing 
+     * from top to bottom.)
+     * 
+     */
     
+
+
     /**
      * Translate row, column index pair to a data array index
      * @param row row index
