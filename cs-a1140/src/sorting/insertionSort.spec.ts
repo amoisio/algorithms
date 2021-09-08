c36841c5e8d75b81a5e4c8679328fc0b92803483
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-09-08 22:53:52 +0300

    Add ability to insert-sort in desc order

--- cs-a1140/src/sorting/insertionSort.spec.ts
+++ cs-a1140/src/sorting/insertionSort.spec.ts
@@ -9,4 +9,12 @@ describe('Insertion sort', () => {
 
         expect(array).toEqual([1, 2, 3, 4, 5, 6]);
     });
+
+    it('Should sort the given array in descending order', () => {
+        const array = [5, 2, 4, 6, 1, 3];
+
+        InsertionSort.sort(array, true);
+
+        expect(array).toEqual([6, 5, 4, 3, 2, 1]);
+    });
 });
--- cs-a1140/src/sorting/insertionSort.ts
+++ cs-a1140/src/sorting/insertionSort.ts
@@ -6,19 +6,21 @@ export default class InsertionSort {
      *  At each iteration i the sub-array [0..i-1] contains the original items [0..i-1]
      *  but in a sorted order.
      * @param array Array to sort.
+     * @param desc Set to true to sort the array in descending order.
      */
-    public static sort<T>(array: T[]): void {
+    public static sort<T>(array: T[], desc: boolean = false): void {
         const len = array.length;
-        for (let i = 0; i < len; i++) {
-            this.sortSubArray(array, i);
+        for (let i = 1; i < len; i++) {
+            this.sortSubArray(array, i, desc);
         }
     }
 
-    private static sortSubArray<T>(array: T[], index: number): void {
+    private static sortSubArray<T>(array: T[], index: number, desc: boolean): void {
         const item = array[index];
         for (let i = index - 1; i >= 0; i--) {
             const comp = array[i];
-            if (item < comp) {
+
+            if ((desc && item > comp) || (!desc && item < comp)) {
                 array[i + 1] = comp;
                 array[i] = item;
             } else {
